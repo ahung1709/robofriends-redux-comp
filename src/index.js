ee1e9b3a607633d7715122d9e3dd0ebff34b11e8
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2024-04-11 12:50:52 -0700

    add logic for redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
+// import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
 
+const logger = createLogger()
 
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store 
+    = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
